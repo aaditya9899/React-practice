4f4c7bb433cade5db91b2218b2feb6f69ada28c5
Aaditya prasad <aadityaprasad9899@gmail.com>
2021-11-16 15:50:18 +0530

    Video 28 completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import Title from './components/Title';
+import Modal from './components/Modal';
 function App() {
+  const [events,setEvents]= useState([
+     
+    {title:"mario's birthday bash",id:1},
+    {title:"bowers live stream",id:2},
+    {title:"race on moo moo",id:3}
+    
+ ])
+
+ const subtitle="All the latest events in Marioland"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Title title="Events in your area" subtitle={subtitle}/>
+   <Title title="Another title" subtitle="Another subtitle"/>
+   {/* <Modal>
+    <h2>
+              10% Off Coupon code!!
+              </h2>
+                <p>
+           Use the code NINJA10 at the checkout.
+                </p> 
+                
+   </Modal> */}
+   <Modal>
+   <h1>Terms and condition</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas eveniet ea tempore libero tenetur reprehenderit sapiente culpa suscipit tempora. Culpa quam alias perferendis beatae natus, quibusdam voluptatibus iste veritatis enim.</p>
+
+   <a href="...">Find out more...</a>
+   </Modal>
+   </div>
   );
 }
 
